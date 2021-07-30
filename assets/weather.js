5f179a92925d80abb5021b687bea4040d61aa721
William Takes <william.e.takes@vanderbilt.edu>
2021-07-30 15:00:01 -0400

    weather.js file updated with ajax

--- assets/weather.js
+++ assets/weather.js
@@ -1,4 +1,22 @@
+var apiKey = "4e5dbe7db2b5e9c8b47fa40b691443d5"
+var city = "raleigh"
+var currentConditions = "https://api.openweathermap.org/data/2.5/weather?appid="
+var fiveDay =
+    "https://api.openweathermap.org/data/2.5/forecast?4e5dbe7db2b5e9c8b47fa40b691443d5q={city name},{country code}"
+var uvIndex =
+    "https://api.openweathermap.org/data/2.5/uvi?appid={appid}&lat={lat}&lon={lon}"
+var searchedArr = JSON.parse(localStorage.getItem("searchedItems")) || [];
 
+
+//taking in user input, and passing the value into a variable
+$(document).ready(function() {
+    $("#search-input").on("click", function(event) {
+    var userInput = $("#city-search").val()
+    console.log(userInput)
+    getWeather(userInput)
+
+    })
+})
 
 
 
@@ -25,3 +43,5 @@ function getWeather(cityName) {
         $("#current-weather").append("<div>" + feelslike + "</div>")
         $("#current-weather").append("<div>" + city + "</div>")
         fiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
+    })
+}
