98af965e89d5a4625947bcde61f0f3c48c4437b7
William Takes <william.e.takes@vanderbilt.edu>
2021-08-10 12:59:45 -0400

    js finished, cannot get info to append to page

--- assets/weather.js
+++ assets/weather.js
@@ -126,19 +126,11 @@ function forecast(cityid){
 
 function addToList(c){
   var listEl= $("<li>"+c.toUpperCase()+"</li>");
-  $(listEl).attr("class","list-group-item");
+  $(listEl).attr("id","list");
   $(listEl).attr("data-value",c.toUpperCase());
-  $(".list-group").append(listEl);
+  $("#list").append(listEl);
 }
-// display the past search again when the list group item is clicked in search history
-function invokePastSearch(event){
-  var liEl=event.target;
-  if (event.target.matches("li")){
-      city=liEl.textContent.trim();
-      currentWeather(city);
-  }
 
-}
 
 function clearHistory(event){
   event.preventDefault();
@@ -150,7 +142,6 @@ function clearHistory(event){
 
 $("#search-input").on("click",displayWeather);
 
-$(window).on("load",loadlastCity);
 $("#clear-history").on("click",clearHistory);
 
 
@@ -158,96 +149,5 @@ $("#clear-history").on("click",clearHistory);
 
 
 
-
-// //taking in user input, and passing the value into a variable
-// $(document).ready(function() {
-//     $("#search-input").on("click", function(event) {
-//     var userInput = $("#city-search").val()
-//     console.log(userInput)
-//     getWeather(userInput)
-
-//     })
-// })
-
-
-
-// function getWeather(cityName) {
-//     var apiCall = ""
-
-//     if (cityName !== "") {
-//         apiCall = currentConditions + apiKey + "&q=" + cityName
-
-//     } else {
-//         apiCall = currentConditions + apiKey + "&q=" + city
-
-//     }
-
-//     $.ajax({
-//         url: apiCall,
-//         method: "GET"
-//     }).then(function(response) {
-//         console.log(response)
-//         var feelslike = response.main.temp
-//         feelslike = (feelslike - 273.15) * 1.8 + 32
-//         feelslike = Math.floor(feelslike)
-//         city = response.name
-//         $("#current-weather").append("<div>" + feelslike + "</div>")
-//         $("#current-weather").append("<div>" + city + "</div>")
-//         fiveDay = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`
-
-
-//         $.ajax({
-//             url: fiveDay,
-//             method: "GET"
-//           }).then(function(response) {
-//             console.log(response)
-      
-//             var averageTemp = 0
-//             var previousdate = ""
-//             var count = 0
-//             var results = 0
-//             previousdate = moment().format("MM/DD/YYYY")
-//             for (let index = 0; index < response.list.length; index++) {
-//               var currentDate = moment(response.list[index].dt, "X").format(
-//                 "MM/DD/YYYY"
-//               )
-//               var temp = response.list[index].main.temp
-//               temp = (temp - 273.15) * 1.8 + 32
-//               temp = Math.floor(temp)
-//               console.log(currentDate)
-//               console.log(temp)
-      
-//               if (previousdate === currentDate) {
-//                 averageTemp = averageTemp + temp
-//                 count++
-//                 previousdate = currentDate
-//               } else {
-//                 results = averageTemp / count
-//                 results = Math.floor(results)
-//                 console.log("results:", results)
-//                 var card = $("<div class = 'card col-sm-2'>")
-      
-//                 var div1 = $("<div class= 'card-header'>")
-//                 div1.append("Date" + '' + currentDate)
-//                 card.append(div1)
-      
-//                 var div2 = $("<div class= 'card-body'>")
-//                 div2.append("Average Temperature:" + results)
-//                 card.append(div2)
-      
-//                 $("#five-day").append(card)
-      
-//                 count = 0
-//                 averageTemp = 0
-//                 previousdate = currentDate
-      
-      
-      
-              
-//               }
-//             }
-//           })
-//         })
-//       }
 
 
